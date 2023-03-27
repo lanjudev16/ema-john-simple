2f8cbaf2a481aa15360d9f50593678b70fa14853
lanjudev16 <lanjudev16@gmail.com>
2023-03-27 21:33:30 +0600

    Cart all most ok

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -24,8 +25,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h4>Order summary</h4>
-                <h2>Total cart is {cart.length}</h2>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
