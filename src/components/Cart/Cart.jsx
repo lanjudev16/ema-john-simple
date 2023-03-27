827cbcfde9955b1c756b2c44065369e4072e4b53
lanjudev16 <lanjudev16@gmail.com>
2023-03-27 23:35:22 +0600

    Done Fix

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,16 +3,19 @@ import './Cart.css'
 const Cart = ({cart}) => {
     let totalPrice=0;
     let totalShipping=0;
+    let quantity=0
     for (const product of cart) {
-        totalPrice=totalPrice+product.price
+        product.quantity=product.quantity||1
+        totalPrice=totalPrice+(product.price)*product.quantity
         totalShipping=totalShipping+product.shipping
+        quantity=quantity+product.quantity
     }
     const tax=totalPrice*7/100
     const grandTotal=totalPrice+totalShipping+tax
   return (
     <div className="cart">
       <h4>Order summary</h4>
-      <p>Selected items: {cart.length}</p>
+      <p>Selected items: {quantity}</p>
       <p>Total price: ${totalPrice.toFixed(2)}</p>
       <p>Shipping: {totalShipping.toFixed(2)}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
